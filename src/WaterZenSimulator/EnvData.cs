81232ed31f4c5e5635d263e6ad417c98f3f47565
ianselmi <ianselmi@isolutions.it>
2023-05-13 22:37:41 +0200

    .

---
+++ src/WaterZenSimulator/EnvData.cs
@@ -0,0 +1,28 @@
+﻿namespace WaterZenSimulator
+{
+    public class RootData
+    {
+        public DateTime Time { get; set; }
+        public EnvData ATC53305e { get; set; }
+        public string TempUnit { get; set; }
+    }
+
+    public class EnvData
+    {
+        public string mac { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public float DewPoint { get; set; }
+        public int Btn { get; set; }
+        public int Battery { get; set; }
+        public int RSSI { get; set; }
+    }
+
+    public class WaterData
+    {
+        public bool WaterOn { get; set; }
+        public float Temperature { get; set; }
+        public float FlowRate { get; set; }
+       
+    }
+}
---
+++ src/WaterZenSimulator/Program.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using WaterZenSimulator.Services.Impl;
+using WaterZenSimulator.Services.Interface;
+
+namespace WaterZenSimulator.Telegram
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(host =>
+                {
+                    host.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ConsoleHostedService>();
+                    services.AddSingleton<IMQTTService, MQTTService>();
+                });
+    }
+
+    internal sealed class ConsoleHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+        private readonly IMQTTService _mqttService;
+
+
+
+        public ConsoleHostedService(
+            ILogger<ConsoleHostedService> logger,
+            IHostApplicationLifetime appLifetime,
+            IMQTTService mqttService)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+            _mqttService = mqttService;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            _logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+            await _mqttService.StartAsync();
+            _appLifetime.ApplicationStarted.Register(async () =>
+            {
+                var isOpenWater = false;
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        Console.ReadKey();
+                        isOpenWater = !isOpenWater;
+                        _resetOpenWater();
+                    }
+
+                    if (isOpenWater)
+                    {
+                        var data = new WaterData
+                        {
+                            FlowRate = r.Next(3, 10),
+                            Temperature = r.Next(40, 50),
+                            WaterOn = false
+                        };
+                        await _mqttService.SendWaterData(data);
+
+                        Console.WriteLine($"Scenario acqua aperta: {JsonSerializer.Serialize(data)}");
+                    }
+                    else
+                    {
+                        var data = new WaterData
+                        {
+                            FlowRate = 0,
+                            Temperature = 20,
+                            WaterOn = false
+                        };
+                        await _mqttService.SendWaterData(data);
+
+                        Console.WriteLine($"Scenario acqua chiusa: {JsonSerializer.Serialize(data)}");
+                    }
+                    await Task.Delay(500);
+                }
+            });
+        }
+
+        private void _resetOpenWater()
+        {
+            
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
