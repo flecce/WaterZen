0f8450b273d91f4b10e49066309120db5e78701e
Fabiano Lecce <f.lecce@isolutions.it>
2023-05-13 17:17:01 +0200

    Settings file added

--- src/WaterZen.Telegram/Program.cs
+++ src/WaterZen.Telegram/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace WaterZen.Telegram
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+namespace WaterZen.Telegram
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var host = CreateHostBuilder(args).Build();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(host =>
+                {
+                    host.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                });
     }
 }
